920d5cb9fa5b46df083536e92b70c6df8621c7ba
RashidMRashidMinhas37 <rashidminhaswebuser37@gmail.com>
2022-02-18 17:48:44 +0200

    New update on Switch Statment project

--- Switch statment/custom.js
+++ Switch statment/custom.js
@@ -52,31 +52,25 @@
 
 
 
-//incomplete
+//use switch to alert my date of birth
 
-let day;
+let day = prompt('Enter your Date of birth like (--/--/--)');
 
+let val = 'Rashid Minhas';
+let val2 = 'Sara Sani';
+let val3 = 'Prince';
 
-switch(new Date().getDay(){
-  case 0:
-    day = 'sunday';
+switch(day){
+  case '9/11/2001','11/9/2001','2001/11/9','9/nov/2001':
+    alert(`Happy Birthday Too you dear ${val}`);
+    document.write(`Hello, ${val} many many returns of the Day `);
     break;
-  case 1:
-    day = 'monday';
-    break;
-  case 2:
-    day = 'tuesday';
-    break;
-  case 3:
-    day = 'wednesday';
-    break;
-  case 4:
-    day = 'thursday';
-     break;
-  case 5:
-     day = 'friday';
-     break;
-  case 6:
-     day = 'saturday';
-    break;
+  case '30/1/2018','1/30/2018','2018/1/30','30/jan/2018':
+    alert(`Happy Birthday Too you dear ${val2}`);
+    document.write(`Hello, ${val2} many many returns of the Day `);
+  case '13/12/1978','12/13/1978','1978/12/13','13/Dec/1978':
+    alert(`Happy Birthday Too you dear ${val3}`);
+    document.write(`Hello, ${val3} many many returns of the Day `);
+    
 }
+
