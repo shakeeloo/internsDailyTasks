65b4751d796ac80eed390a21a8a0957d1afb9984
Adnan Ali <adnanameerzzz@gmail.com>
2022-02-21 04:05:25 -0800

    Daily task- Learing about loops

---
+++ 20th-Feb-2022 -b/Loops/App.js
@@ -0,0 +1,82 @@
+
+// For Loop 
+
+// const randomNames = ["red","blue","green","yellow","Bannana", "Apple", "Orange"]
+
+
+//     for (let i = 0; i < randomNames.length; i++) {
+        
+//         if(i == 0){
+//         console.log("red")
+//         continue;
+//         }
+//         if(i == 1){
+//             console.log(`Blue`)
+//         continue;
+                
+//         }
+//         if(i == 2){
+//             console.log(`Green`)
+//         continue;
+                
+//         }
+//         if(i == 3){
+//             console.log(`yellow`)
+//         continue;
+                
+//         }
+//         if(i == 4){
+//             console.log(`Bannana`)
+//         continue;
+                
+//         }
+//         if(i == 5 ){
+//             console.log(`Apple`)
+//         continue;
+                
+//         }
+             
+//     }
+
+
+
+
+    // FOR IN LOOP 
+
+    // const randomNames = {
+    //     name: "adnan",
+    //     location: "hunza",
+    //     profession: "js dev"
+    // }
+    // for(const key in randomNames){
+    //     // console.log(randomNames[key])
+    //     console.log(`Here is my ${key} "${randomNames[key]}" `)
+    // }
+  
+
+
+    // FOR OF LOOP 
+
+    // const anArray = ['number1','number2','number3','number4','number5','number6','number7',]
+
+    //     for(const key of anArray){
+    //         console.log(key)
+    //     }
+
+
+
+
+    // FOR EACH LOOP 
+
+    // let anArray = ['local1','local2','local3','local4','local5','local6',]
+
+    // anArray.forEach(function(currValue) {
+    //         console.log(`This is ${currValue}`)
+    // })
+
+    // let theArray = ['laptop1','laptop2','laptop3','laptop4','laptop5','laptop6',]
+
+    //     theArray.forEach(function(index, CarName){
+    //         console.log(`${index} : ${CarName}`)
+    //     })
+
