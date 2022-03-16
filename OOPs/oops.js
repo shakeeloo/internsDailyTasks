2d16f933380a0e184c698222f09c74688dd4372a
massoGit-ma <almashayatjaved@gmail.com>
2022-03-16 11:36:13 -0700

    commiting more

--- OOPs/oops.js
+++ OOPs/oops.js
@@ -25,66 +25,99 @@
 
                 //this key word
 
-let me = {
-name: "Almas",
-age: 40,
+// let me = {
+// name: "Almas",
+// age: 40,
                   
-sayHello() {
-// "this" is the "current object"
-alert(` Hello ${this.name}`);               // if you don't mention this keyword here , you'll get nothing in output
+// sayHello() {
+// // "this" is the "current object"
+// alert(` Hello ${this.name}`);               // if you don't mention this keyword here , you'll get nothing in output
+// }
+                  
+// };
+                  
+// me.sayHello(); // Almas
+
+//                         // Javascript Object Prototype
+
+// // constructor function
+// function Person (name) {
+//     this.name = name,
+//     this.age = 15
+// }
+
+// // creating objects
+// let person_1 = new Person('Asma');
+// let person_2 = new Person('Usra');
+
+// // we can add new property to constructor function
+// Person.prototype.relation = 'Sister';
+
+// console.log(person_1.relation); 
+// console.log(person_2.relation);   // the output for these codes will be 'male'              
+
+// // Customer constructor
+
+// function Todo(priority1, priority2) {
+//     this.priority1 = priority1;
+//     this.priority2 = priority2;
+//   }
+  
+
+// function Routine(priority1, priority2, interest1, interest2) {
+//     Todo.call(this, priority1, priority2);
+  
+//     this.interest1 = interest1;
+//     this.interest2 = interest2;
+//   }
+  
+//   // here we Inherit the Todo prototype methods
+//   Routine.prototype = Object.create(Todo.prototype);
+  
+//   //  now we make routine.prototype return Routine()
+//   Routine.prototype.constructor = Routine;
+  
+//   // Here we are creating a new routine using constructor
+//   const rout1 = new Routine('studying', 'managing time', 'Skating', 'Cycling');
+  
+//   console.log(rout1);
+  
+//   // Showing Routine progress by making a new function
+//   Routine.prototype.progress = function(){
+//     return `Yeahooooo I am done with ${this.priority1}  and ${this.priority2} `;
+//   }
+  
+//   console.log(rout1.progress());
+
+
+            //Prototype not only adds new method or new property to the constructor but it
+            // can manipulate the object also
+
+function Visited(firstPlace,secondPlace,thirdPlace,fourthPlace){
+    this.firstPlace = firstPlace;
+    this.secondPlace = secondPlace;
+    this.thirdPlace = thirdPlace;
+    this.fourthPlace = fourthPlace;
 }
-                  
-};
-                  
-me.sayHello(); // Almas
-
-                        // Javascript Object Prototype
-
-// constructor function
-function Person (name) {
-    this.name = name,
-    this.age = 15
-}
-
-// creating objects
-let person_1 = new Person('Asma');
-let person_2 = new Person('Usra');
-
-// we can add new property to constructor function
-Person.prototype.relation = 'Sister';
-
-console.log(person_1.relation); 
-console.log(person_2.relation);   // the output for these codes will be 'male'              
-
-// Customer constructor
-
-function Todo(priority1, priority2) {
-    this.priority1 = priority1;
-    this.priority2 = priority2;
+// Get full name
+Visited.prototype.startedJourney = function(){
+    return new Date('3-14-2005')
   }
   
-
-function Routine(priority1, priority2, interest1, interest2) {
-    Todo.call(this, priority1, priority2);
-  
-    this.interest1 = interest1;
-    this.interest2 = interest2;
+  // Gets Married
+  Visited.prototype.changePlace = function(secondPlace){
+    this.secondPlace = secondPlace;
   }
   
-  // here we Inherit the Todo prototype methods
-  Routine.prototype = Object.create(Todo.prototype);
+  const first = new Visited('Portugal', 'Paris', 'Morocow','indulusia');
+  const second = new Visited('India', 'Singapore', 'Egypt','turkey');
   
-  //  now we make routine.prototype return Routine()
-  Routine.prototype.constructor = Routine;
+  console.log(second);
   
-  // Here we are creating a new routine using constructor
-  const rout1 = new Routine('studying', 'managing time', 'Skating', 'Cycling');
+  console.log(first.startedJourney());
   
-  console.log(rout1);
+  console.log(second.startedJourney());
   
-  // Showing Routine progress by making a new function
-  Routine.prototype.progress = function(){
-    return `Yeahooooo I am done with ${this.priority1}  and ${this.priority2} `;
-  }
+  second.changePlace('Europe');
   
-  console.log(rout1.progress());
+  console.log(second.startedJourney());
